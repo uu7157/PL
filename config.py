ed223e7e419ac57c264f1f1fe802be771b9775e0
uu7157 <116019430+uu7157@users.noreply.github.com>
2023-04-22 02:12:43 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    BOT_API_ID = <insert your bot API ID here>
+    BOT_API_HASH = '<insert your bot API hash here>'
+    BOT_TOKEN = '<insert your bot token here>'
+    CHANNEL_NAME = '<insert the name of your Telegram channel here>'
