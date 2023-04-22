f7820ad8bd2dfb72b9aebc0bcb29b62edb10648b
uu7157 <116019430+uu7157@users.noreply.github.com>
2023-04-22 03:00:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import os
 
 PASTES_FILE = 'pastes.txt'
 
-def scrape_pastelink():
+def scrape_pastelink() -> List[str]:
     url = 'https://pastelink.net/recent'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     table = soup.find('table', id='listing')
     rows = table.find_all('tr')[1:]  # skip the header row
-    paste_content = ''
     scraped_urls = set()
     if os.path.exists(PASTES_FILE):
         with open(PASTES_FILE, 'r') as f:
             scraped_urls = set(f.read().splitlines())
+    messages = []
     for row in rows:
         cols = row.find_all('td')
         paste_url = cols[0].find('a')['href']
@@ -31,15 +31,17 @@ def scrape_pastelink():
         paste_response = requests.get(paste_url)
         paste_soup = BeautifulSoup(paste_response.content, 'html.parser')
         paste_body = paste_soup.find('div', id='body-display').text.strip()
-        paste_content += f'<b>{paste_title}</b> ({paste_views} views, {paste_age})\n\n{paste_body}\n\n'
+        message = f'<b>{paste_title}</b> ({paste_views} views, {paste_age})\n\n{paste_body}\n\n'
+        messages.append(message)
         scraped_urls.add(paste_url)
     with open(PASTES_FILE, 'w') as f:
         f.write('\n'.join(scraped_urls))
-    return paste_content
+    return messages
 
 def send_to_telegram(client: Client, message: Message):
-    paste_content = scrape_pastelink()
-    client.send_message(chat_id=Config.CHANNEL_ID, text=paste_content, parse_mode='HTML')
+    messages = scrape_pastelink()
+    for message in messages:
+        client.send_message(chat_id=Config.CHANNEL_ID, text=message, parse_mode='HTML')
 
 if __name__ == '__main__':
     client = Client(
